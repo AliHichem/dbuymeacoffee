388d0005deacfb43c5ed73ef937c10b40aa48b66
hichem ali <ali.hichem@mail.com>
2023-11-02 15:25:58 +0100

    better SmartContract error coverage

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -38,12 +38,25 @@ export const getError = (error: any): [number, string] => {
     let _code: number = 0;
     let _message: string = "";
     let _error: any = null;
+    let _smFound: boolean = false;
+    const token = '##';
     if (error instanceof BaseError) {
-        const revertError: Error|null = error.walk(err => err instanceof ContractFunctionRevertedError)
-        const executionError: Error|null = error.walk(err => err instanceof ContractFunctionExecutionError)
-        if(executionError instanceof ContractFunctionExecutionError) {
+        const revertError: Error | null = error.walk(err => err instanceof ContractFunctionRevertedError)
+        const executionError: Error | null = error.walk(err => err instanceof ContractFunctionExecutionError)
+        if (executionError instanceof ContractFunctionExecutionError) {
             _code = 2;
-            _message = executionError?.shortMessage?.split("##")[1];
+            if (executionError?.details) {
+                _message = executionError?.details;
+                if (_message.includes(token)) {
+                    _smFound = true;
+                    _message = _message.split("##")[1];
+                }
+            } else if (executionError?.shortMessage && !_smFound) {
+                _message = executionError?.shortMessage;
+                if (_message.includes(token)) {
+                    _message = executionError?.shortMessage?.split("##")[1]
+                }
+            }
         } else if (revertError instanceof ContractFunctionRevertedError && revertError?.data?.args) {
             _code = 1;
             // @ts-ignore
@@ -55,9 +68,11 @@ export const getError = (error: any): [number, string] => {
         // keep all string after the first "##"
         _message = _error?.split("##")[1];
     }
+    console.log('>>>>> error.constructor.name <<<<', error.constructor.name);
     //if _message is empty or undefined throw full error
     if (_message === undefined || _message === "") {
-        console.log('@@@@ Vanilla Js Error: ',error);
+        console.log('@@@@ Vanilla Js Error: ', error);
+        alert(error);
     }
     return [_code, _message];
 }
